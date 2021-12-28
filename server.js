c881ee12c1b46eb41bdbc377b5a7aac439eb44d7
khaoula <khaoulajabour4@gmail.com>
2021-12-28 01:42:37 -0500

    add note

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
-const express = require('express');
 const PORT = process.env.PORT || 3001;
+const express = require('express');
+const fs = require('fs');
 const app = express();
 const path = require('path');
-const {notes} = require('./db/db.json')
+const {notes} = require('./db/db.json');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.use(express.static('public'));
+
+function createNote(body, notesArr){
+    body.id = Date.now().toString();
+    const note = body;
+    notesArr.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({notes : notesArr}, null, 2)
+    );
+    return note;
+
+}
+
 
 //api routes
 app.get('/api/notes', (req, res)=>{
     res.json(notes)
+});
+
+app.post('/api/notes',(req, res)=>{
+    if(!req.body){
+        console.log('error');
+    }
+    else{
+        const note = createNote(req.body, notes);
+        res.json(note);
+        console.log(note);
+    }
 });
 
 //html routes
